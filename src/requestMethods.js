1a807f554553ece97bd0607dd7358a17bfd70136
Chaos <antonioscutari2@hotmail.com>
2022-07-03 18:08:12 -0300

    [CHANGE] Token method

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOWI0YTEwMDQ1YTE3NTJkNmI4ZWU5MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjQwOTMwNiwiZXhwIjoxNjU2NjY4NTA2fQ.wkSoOiY2UXgNrKO3RoGhky3md3iIH6f6i5o3eIA9kNc";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 
