b55425c2acbb38dfe815e1613ecde70158053800
Chaos <antonioscutari2@hotmail.com>
2022-07-07 18:52:03 -0300

    [ADD] Methods. [¿LEFT?] I need to check

--- admin/src/redux/apiCall.js
+++ admin/src/redux/apiCall.js
@@ -1,5 +1,18 @@
-import { useDispatch } from "react-redux";
-import { publicRequest } from "../requestMethods";
+import { publicRequest, userRequest } from "../requestMethods";
+import {
+  addProductFailure,
+  addProductStart,
+  addProductSuccess,
+  deleteProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+  getProductFailure,
+  getProductStart,
+  getProductSuccess,
+  updateProductFailure,
+  updateProductStart,
+  updateProductSuccess,
+} from "./productRedux";
 import { loginFailure, loginStart, loginSuccess } from "./userSlice";
 
 export const login = async (dispatch, user) => {
@@ -9,5 +22,44 @@ export const login = async (dispatch, user) => {
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
+  }
+};
+
+export const getProducts = async (dispatch) => {
+  dispatch(getProductStart());
+  try {
+    const res = await publicRequest.get("/products");
+    dispatch(getProductSuccess(res.data));
+  } catch (err) {
+    dispatch(getProductFailure());
+  }
+};
+
+export const deleteProduct = async (id, dispatch) => {
+  dispatch(deleteProductStart());
+  try {
+    // const res = await userRequest.delete(`/products/${id}`);
+    dispatch(deleteProductSuccess(id));
+  } catch {
+    dispatch(deleteProductFailure());
+  }
+};
+
+export const updateProduct = async (id, product, dispatch) => {
+  dispatch(updateProductStart());
+  try {
+    dispatch(updateProductSuccess({ id, product }));
+  } catch {
+    dispatch(updateProductFailure());
+  }
+};
+
+export const addProduct = async (product, dispatch) => {
+  dispatch(addProductStart());
+  try {
+    const res = await userRequest.post(`/products`, product);
+    dispatch(addProductSuccess(res.data));
+  } catch {
+    dispatch(addProductFailure());
   }
 };
