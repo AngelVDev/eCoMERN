644e6d23e0ab2d47fda04535c509830da4af26df
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-10 21:58:02 -0300

    [FIX] Store bug.

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,15 +22,17 @@ const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = () =>
-  configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-  });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export let persistor = persistStore(store);
+export let persistor = newFunction();
+function newFunction() {
+  return persistStore(store);
+}
