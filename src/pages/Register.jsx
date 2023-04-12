c02114ec722b07e8e597d11d010d59e816563616
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-11 22:10:25 -0300

    Uppercase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -60,12 +60,12 @@ const Register = () => {
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input placeholder='Name' />
+          <Input placeholder='Last name' />
+          <Input placeholder='Username' />
+          <Input placeholder='E-mail' />
+          <Input placeholder='Password' />
+          <Input placeholder='Confirm password' />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
