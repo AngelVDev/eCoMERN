327a59cf360191ed911124b49b9c5b61ee685ff6
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-12 23:30:23 -0300

    [ADD] Funtionality to form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRegister } from "../redux/apiCall";
 
 const Container = styled.div`
   width: 100vw;
@@ -55,22 +61,105 @@ const Button = styled.button`
 `;
 
 const Register = () => {
+  const dispatch = useDispatch();
+  const navigate = useHistory();
+  // const errorBack = useSelector((state) => state.user.error_register);
+  const registerDone = useSelector((state) => state.user.register_done);
+  // const [errors, setErrors] = useState("");
+  const [input, setInput] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (registerDone) {
+      alert("Usuario creado correctamente");
+      setInput({
+        userName: "",
+        email: "",
+        password: "",
+        password2: "",
+      });
+      return navigate.push("/home");
+    }
+  }, [dispatch, navigate, registerDone]);
+
+  // const inputsError = ["userName", "email", "password", "password2"];
+
+  function handleChange(e) {
+    e.preventDefault();
+
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    // setErrors(
+    //   validate({
+    //     ...input,
+    //     [e.target.name]: e.target.value,
+    //   })
+    // );
+    // dispatch(resetError());
+  }
+  // function handleChange2(e) {
+  //   e.preventDefault();
+  //   setErrors(
+  //     validate({
+  //       ...input,
+  //       [e.target.name]: e.target.value,
+  //     })
+  //   );
+  // }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // if (
+    //   !inputsError.some((inp) => errors.hasOwnProperty(inp)) &&
+    //   input.userName.length > 0
+    // ) {
+    dispatch(userRegister(input));
+    // }
+  }
+
   return (
     <Container>
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder='Name' />
-          <Input placeholder='Last name' />
-          <Input placeholder='Username' />
-          <Input placeholder='E-mail' />
-          <Input placeholder='Password' />
-          <Input placeholder='Confirm password' />
+          {/* <Input placeholder='Name' />
+          <Input placeholder='Last name' /> */}
+          <Input
+            placeholder='Username'
+            name='userName'
+            value={input.value}
+            onChange={handleChange}
+          />
+          <Input
+            placeholder='E-mail'
+            name='email'
+            value={input.value}
+            onChange={handleChange}
+          />
+          <Input
+            placeholder='Password'
+            type='password'
+            name='password'
+            value={input.value}
+            onChange={handleChange}
+          />
+          <Input
+            placeholder='Confirm password'
+            type='password'
+            name='password2'
+            value={input.value}
+            onChange={handleChange}
+          />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button>CREATE</Button>
+          <Button onClick={handleSubmit}>CREATE</Button>
         </Form>
       </Wrapper>
     </Container>
