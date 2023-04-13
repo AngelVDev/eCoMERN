a3065f90c641169b887641ece185be04479b5bd4
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-13 19:49:48 -0300

    [ADD] Info export of currentUser

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,8 +3,12 @@ const BASE_URL = "https://e-commerce-tru.onrender.com/api";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
-
+export const TOKEN = currentUser?.accessToken;
+export const currentUserInfo = {
+  username: currentUser.username,
+  email: currentUser.email,
+  isAdmin: currentUser.isAdmin,
+};
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 
 export const userRequest = axios.create({
