7dbe128626ea1d511994ba232961bb8ed4cdf2f1
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-14 23:37:07 -0300

    [FIX] Imports

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,21 +1,17 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import Login from "./pages/login/Login";
+import Topbar from "./components/topBar/TopBar";
+import Sidebar from "./components/sideBar/SideBar";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
-import Login from "./pages/login/Login";
-import { useSelector } from "react-redux";
 
 function App() {
   const admin = useSelector((state) => state.user.currentUser.isAdmin);
